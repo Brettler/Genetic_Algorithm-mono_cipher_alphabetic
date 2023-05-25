182e4d9dc65ad720dad5b19babb9319e1bcc2e3f
brettler <liad.brettler@gmail.com>
2023-05-25 16:43:09 +0300

    GeneticAlgorithmV2.py and gui.py are updated and work.

--- gui.py
+++ gui.py
@@ -1,11 +1,12 @@
 import tkinter as tk
-import GeneticAlgorithm
+import GeneticAlgorithmV2
 from tkinter import ttk
 import threading
 import queue
 import sys
-message_queue = []  # Store the messages
-current_message_index = 0  # Index of the current message being displayed
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+from matplotlib.figure import Figure
+
 """
 def check_queue(q):
     try:
@@ -18,6 +19,9 @@ def check_queue(q):
 
     root.after(1, check_queue, q)  # check the queue again after 100 ms
 """
+
+message_queue = []  # Store the messages
+current_message_index = 0  # Index of the current message being displayed
 
 def check_queue(q):
     global current_message_index
@@ -45,7 +49,21 @@ def on_close():
     root.destroy()
     sys.exit()
 
+
+
+
+
 def run_genetic_algorithm():
+    global current_message_index
+
+    # Clear the figure, message queue and output text
+    fig.clf()
+    message_queue.clear()
+    output_text.delete(1.0, tk.END)
+    current_message_index = 0  # Reset the current message index
+
+
+
     cipher_text_path = 'enc.txt'
     with open(cipher_text_path, 'r') as f:
         cipher_text = f.read().strip()
@@ -60,19 +78,22 @@ def run_genetic_algorithm():
 
     #GeneticAlgorithm.genetic_algorithm(cipher_text, optimization, population_size, max_mutation_rate, min_mutation_rate, max_iterations, elitism)
     q = queue.Queue()
-    thread = threading.Thread(target=GeneticAlgorithm.genetic_algorithm, args=(q, cipher_text, optimization, population_size, max_mutation_rate, min_mutation_rate, max_iterations, elitism))
+    thread = threading.Thread(target=GeneticAlgorithmV2.genetic_algorithm, args=(q, fig, canvas, cipher_text, optimization, population_size, max_mutation_rate, min_mutation_rate, max_iterations, elitism))
     thread.daemon = True
     thread.start()
     check_queue(q)  # check the queue for messages right away
 
 root = tk.Tk()
 
+fig = Figure(figsize=(5, 4), dpi=100)
+canvas = FigureCanvasTkAgg(fig, master=root)  # A tk.DrawingArea.
+canvas.get_tk_widget().pack(side=tk.TOP, fill=tk.BOTH, expand=1)
 
 population_size_label = tk.Label(root, text="Population number:")
 population_size_label.pack()
 population_size_entry = tk.Entry(root)
 population_size_entry.pack()
-population_size_entry.insert(0, "120")
+population_size_entry.insert(0, "200")
 
 max_mutation_rate_label = tk.Label(root, text="Mutation rate:")
 max_mutation_rate_label.pack()
@@ -84,13 +105,13 @@ min_mutation_rate_label = tk.Label(root, text="Minimum mutation rate:")
 min_mutation_rate_label.pack()
 min_mutation_rate_entry = tk.Entry(root)
 min_mutation_rate_entry.pack()
-min_mutation_rate_entry.insert(0, "0.05")
+min_mutation_rate_entry.insert(0, "0.02")
 
 max_iterations_label = tk.Label(root, text="Maximum iterations:")
 max_iterations_label.pack()
 max_iterations_entry = tk.Entry(root)
 max_iterations_entry.pack()
-max_iterations_entry.insert(0, "1000")
+max_iterations_entry.insert(0, "300")
 
 elitism_label = tk.Label(root, text="Elitism:")
 elitism_label.pack()
@@ -107,9 +128,8 @@ optimization_combobox.current(0)  # set initial selection to "None"
 
 run_button = tk.Button(root, text="Run", command=run_genetic_algorithm)
 run_button.pack()
-
 # We choose the width by the amount of characters.
-output_text = tk.Text(root, width=100)
+output_text = tk.Text(root, width=130)
 output_text.pack()
 
 root.protocol("WM_DELETE_WINDOW", on_close)
